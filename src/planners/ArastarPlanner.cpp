a050eef5b97742ca1f8ea6781cd9e94c40ea97f3
Hanlan Yang <yanghanlan666@gmail.com>
2023-03-29 16:49:35 -0400

    Change planner stats var name for logging

--- src/planners/ArastarPlanner.cpp
+++ src/planners/ArastarPlanner.cpp
@@ -42,8 +42,8 @@ bool ArastarPlanner::Plan()
             data_list.push_back(heuristic_w_);
             data_list.push_back(1e-9*t_elapsed);
             data_list.push_back(goal_state_ptr_->GetFValue());
-            data_list.push_back(planner_stats_.num_state_expansions_);
-            data_list.push_back(planner_stats_.num_evaluated_edges_);
+            data_list.push_back(planner_stats_.num_state_expansions);
+            data_list.push_back(planner_stats_.num_evaluated_edges);
         }
         // Early termination if there's no solution
         if (goal_state_ptr_ == NULL || heuristic_w_ == 1)
