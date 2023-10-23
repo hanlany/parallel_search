c4bb44d7ffdfdfb80644a830c250e17dc6ca3c96
Hanlan Yang <yanghanlan666@gmail.com>
2023-10-23 18:47:59 -0400

    Gen random cost factor script minor change

--- examples/robot_nav_2d/resources/gen_cost_factor.py
+++ examples/robot_nav_2d/resources/gen_cost_factor.py
@@ -2,11 +2,19 @@ import numpy as np
 import os
 import pdb
 
-dim = {'hrt201n': [305, 294], 'den501d': [338, 320], 'den520d': [257, 256],  'ht_chantry':  [141, 162], 'brc203d': [391, 274]}
+dim1 = {'hrt201n': [305, 294], 'den501d': [338, 320], 'den520d': [257, 256], 'brc203d': [391, 274]}
+scale1 = 2
 
-scale = 5
+dim2 = {'ht_chantry':  [141, 162]}
+scale2 = 4
 
-for map_name, dims in dim.items():
-	print("Map: {} | dims: {}".format(map_name, np.multiply(dims,scale)))
-	cost_mat = np.random.uniform(low=1, high=100, size=np.multiply(dims,scale))
+
+for map_name, dims in dim1.items():
+	print("Map: {} | dims: {}".format(map_name, np.multiply(dims,scale1)))
+	cost_mat = np.random.uniform(low=1, high=100, size=np.multiply(dims,scale1))
+	np.savetxt(os.path.join(map_name, map_name +'_cost_factor.map'), cost_mat, fmt='%d')
+
+for map_name, dims in dim2.items():
+	print("Map: {} | dims: {}".format(map_name, np.multiply(dims,scale2)))
+	cost_mat = np.random.uniform(low=1, high=100, size=np.multiply(dims,scale2))
 	np.savetxt(os.path.join(map_name, map_name +'_cost_factor.map'), cost_mat, fmt='%d')
